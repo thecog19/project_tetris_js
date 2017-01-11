e4895fe3a7b661d44a8c172df15d46eb8ee46860
Alexa <alexa.anderson@outlook.com>
2017-01-11 16:24:38 -0600

    Debuggin

--- js/model.js
+++ js/model.js
@@ -112,17 +112,26 @@ var model = TETRIS.model = {
       if(row === 10){ 
         completedRows.push(index)
         model.board.resetState(index)
-        }       
-      });
+      }       
+    });
       
-      for (var i = model.board.blockArray.length - 1; i >= 0; i--){
-        console.log("I'm checking index " + i)
-        console.log(completedRows)
-        if(completedRows.includes(model.board.blockArray[i].y)){
-          model.board.blockArray.splice(i, 1)
+    for (var i = model.board.blockArray.length - 1; i >= 0; i--){
+      if(completedRows.includes(model.board.blockArray[i].y)){
+        model.board.blockArray.splice(i, 1)
+      }
+    };
+    for (var j = 0; j < model.board.blockArray.length; j++) {
+      completedRows.forEach(function(row) {
+        if (model.board.blockArray[j].y < row) {
+          model.board.blockArray[j].y += 1;
+          model.board.boardState[row - 1] -= 1;
+          console.log('increasing row ' + row)
+          console.log('block y ' + model.board.blockArray[j].y)
+          model.board.boardState[row] += 1;
         }
-      };
-    
+      })
+    }
+  
   }
 
 
